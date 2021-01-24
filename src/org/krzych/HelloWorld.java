cfdc6083a2245081629b522611de5d6630f6c99e
pirx <pirx@flash>
2021-01-24 19:33:44 +0100

    Poczakowy commit

---
+++ src/org/krzych/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.krzych;
+import com.krzych.*;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		helloPrint hp = new helloPrint(); 
+		System.out.println("Hello"+hp.printHello());
+	}
+
+}
