6398b247f40ccc1c093186cdcad69df9eaba75e0
pirx <pirx@flash>
2021-01-24 20:05:16 +0100

    dodalem linie "Kolejny test"

--- src/org/krzych/HelloWorld.java
+++ src/org/krzych/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		helloPrint hp = new helloPrint(); 
 		System.out.println("Hello"+hp.printHello());
+		System.out.println("No zaczynamy przygotowania i nauke:)");
+		System.out.println("KOlejny test");
 	}
 
 }
